06a900fcd03a4c03b1c009e00515960e85bb918d
johnlee0402 <johnlee940402@gmail.com>
2025-03-11 12:01:24 +0800

    Add files via upload

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "Hello 李孟翰"
+
+@app.route("/mis")
+def course():
+	return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+now = datetime.now()
+return render_template("today.html", datetime = str(now))
+
+if __name__ == "__main__":
+	app.run()
